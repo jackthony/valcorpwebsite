4a2ad7ac334e32e0aca6a6c49424431a35b5c80c
Abelsirius <abelgm.crack@gmail.com>
2024-07-05 01:06:36 -0500

    avances de las secciones de los proyectos

--- valcorpwebsite/src/app/app.config.ts
+++ valcorpwebsite/src/app/app.config.ts
@@ -1,10 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideClientHydration(), provideAnimationsAsync()]
+  providers:
+    [provideRouter(
+      routes, withComponentInputBinding(),withViewTransitions())
+      , provideClientHydration(),
+    provideAnimationsAsync()]
 };
--- valcorpwebsite/src/app/landing/componentes/ubicacion/ubicacion.component.ts
+++ valcorpwebsite/src/app/landing/componentes/ubicacion/ubicacion.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-ubicacion',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './ubicacion.component.html',
   styleUrl: './ubicacion.component.css'
 })
 export class UbicacionComponent {
+
+     imagen = input<string>();
+     color = input<string>();
 
 }
