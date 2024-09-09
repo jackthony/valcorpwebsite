21c82e1f5f6d334cd536f6a27b71f4e42f7db038
Abelsirius <abelgm.crack@gmail.com>
2024-09-09 15:02:50 -0500

    optimizacion

--- valcorpwebsite/src/app/landing/pages/home/home.component.ts
+++ valcorpwebsite/src/app/landing/pages/home/home.component.ts
@@ -3,13 +3,25 @@ import { HeaderComponent } from "../../componentes/header/header.component";
 import { ProyectoComponent } from '../../componentes/proyecto/proyecto.component';
 import { ProyectosComponent } from "../../proyectos/proyectos.component";
 import { FamiliaComponent } from "../../familia/familia.component";
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { NgIf } from '@angular/common';
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent, ProyectosComponent, FamiliaComponent]
+    imports: [HeaderComponent, ProyectosComponent, FamiliaComponent,MatProgressSpinner,NgIf]
 })
 export default class HomeComponent {
+    loading: boolean = true; // Controla la visibilidad del spinner
 
+    onVideoLoad(): void {
+    console.log('ya cargo')
+      this.loading = false; // Oculta el spinner cuando el video esté listo
+    }
+  
+    onVideoError(): void {
+      this.loading = false; // Oculta el spinner en caso de error
+      console.error('Error al cargar el video');
+    }
 }
