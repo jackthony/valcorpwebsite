53ebbefd7e5a19a0ac7fb723b79e0c86061a1e3d
AxelValuis <banel1306@hotmail.es>
2024-10-08 19:52:53 -0500

    Actualizacion.2

--- valcorpwebsite/tailwind.config.js
+++ valcorpwebsite/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/app/*/.landing.{html,ts}'],
   theme: {
     extend: {
       fontFamily: {
